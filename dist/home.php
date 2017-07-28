660f69e84790e8e8c976c452aa41e6bb06b48c3b
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-07-28 09:57:20 +0200

    First commit

---
+++ dist/home.php
@@ -0,0 +1,9 @@
+<?php include "inc/head.inc"; ?>
+<body style="background-color: #999;">
+	<div class="nav-bar white">
+		<?php include "inc/nav.inc"; ?>
+	</div>
+	<header class="main-header">
+		
+	</header>
+</body>
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+
+gulp.task('images', function () {
+    gulp.src('src/images/**/*')
+        .pipe(cache(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('styles', function () {
+    gulp.src(['src/scss/**/*.scss'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass({
+            includePaths: ['bower_components/foundation-sites/scss', 'bower_components/motion-ui/src/'],
+        }))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'))
+});
+
+gulp.task('default', function () {
+    gulp.watch("src/scss/**/*.scss", ['styles']);
+    gulp.watch("src/js/**/*.js", ['scripts']);
+});
