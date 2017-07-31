9aec8350b428fd28c50652d686e0f9f40becf044
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-07-31 14:56:46 +0200

    More sections in homepage

--- dist/home.php
+++ dist/home.php
@@ -160,10 +160,125 @@
 				<div><a href="#">Contacte <i class="fa fa-chevron-right"></i></a></div>
 			</div>
 		</div>
-		<div class="primary-triangle-background"></div>
+		<div class="primary-triangle-background-top"></div>
 	</section>
-	<section class="home-featured section-padding section-primary">
-		
+	<section class="home-featured-section section-padding section-primary">
+		<div class="home-featured-section-title section-title row">
+			<div class="column text-center">
+				<h2 class="relative"><span class="white-left-line"></span>Destacats</h2>
+			</div>
+		</div>
+		<div class="home-featured-section-block">
+			<div class="row collapse small-up-1 large-up-4">
+				<div class="home-featured-block-item column">
+					<div class="teaser-date">
+						<div class="teaser-date-day">DIM</div>
+						<div class="teaser-date-day-month">28 Jul</div>
+					</div>
+					<div class="teaser-featured-image">
+						<img src="#" alt="#">
+					</div>
+					<div class="teaser-body">
+						<h2>Vida i miracles de n'Aineta dels matalassos</h2>
+						<div class="black-bold field-category">Teatre</div>
+						<p>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Proin eget tortor risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
+	<section class="home-news-section section-padding relative">
+		<div class="primary-triangle-background-bottom"></div>
+		<div class="home-news-section-titles section-title row relative">
+			<div class="column text-center">
+				<h2>Notícies</h2>
+			</div>
+		</div>
+		<div class="home-news-block row section-padding align-center">
+			<div class="medium-10 columns">
+				<div class="row collapse">
+					<div class="home-news-block-item small-12 medium-4 column row collapse flex">
+						<div class="teaser-image small-4 medium-12 columns">
+							<img src="images/placeholder/news_placeholder.jpg" alt="">
+						</div>
+						<div class="teaser-body small-7 medium-12 columns grid-x grid-padding-x align-middle">
+							<div class="cell">
+								<h2>Vivamus suscipit tortor eget</h2>
+								<p>Sed porttitor lectus nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+						</div>
+						<div class="teaser-link small-1 show-for-small-only columns grid-x grid-padding-x align-middle">
+							<div class="cell">
+								<a href="#"><i class="fa fa-chevron-right"></i></a>
+							</div>
+						</div>
+					</div>
+					<div class="home-news-block-item small-12 medium-4 column row collapse flex">
+						<div class="teaser-image small-4 medium-12 columns">
+							<img src="images/placeholder/news_placeholder.jpg" alt="">
+						</div>
+						<div class="teaser-body small-7 medium-12 columns grid-x grid-padding-x align-middle">
+							<div class="cell">
+								<h2>Vivamus suscipit tortor eget</h2>
+								<p>Sed porttitor lectus nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+						</div>
+						<div class="teaser-link small-1 show-for-small-only columns grid-x grid-padding-x align-middle">
+							<div class="cell">
+								<a href="#"><i class="fa fa-chevron-right"></i></a>
+							</div>
+						</div>
+					</div>
+					<div class="home-news-block-item small-12 medium-4 column row collapse flex">
+						<div class="teaser-image small-4 medium-12 columns">
+							<img src="images/placeholder/news_placeholder.jpg" alt="">
+						</div>
+						<div class="teaser-body small-7 medium-12 columns grid-x grid-padding-x align-middle">
+							<div class="cell">
+								<h2>Vivamus suscipit tortor eget</h2>
+								<p>Sed porttitor lectus nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+						</div>
+						<div class="teaser-link small-1 show-for-small-only columns grid-x grid-padding-x align-middle">
+							<div class="cell">
+								<a href="#"><i class="fa fa-chevron-right"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="home-email-section section-padding section-primary">
+		<div class="home-email-section-inner">
+			<div class="home-email-block row align-middle align-center">
+				<div class="home-email-block-content medium-4 columns">
+					<form id="home-email-form" method="post" action="">
+						<div class="input-group">
+							<input type="email" class="input-group-field" placeholder="Correu electrònic" name="">
+							<div class="input-group-button">
+								<button type="submit" class="button"><i class="fa fa-chevron-right"></i></button>
+							</div>
+						</div>
+						<p class="description">Vols rebre totes les novetats de l’Auditori d’Alcúdia?</p>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer class="main-footer section-padding section-dark">
+		<div class="main-footer-inner">
+			<div class="main-footer-top row"></div>
+			<hr>
+			<div class="main-footer-bottom row">
+				<div class="medium-6 columns text-left"></div>
+				<div class="medium-6 columns text-right">
+					<p><a href="#">Contacte</a></p>
+					<p><address>Adreça del teatre</address><br>
+					<a href="tel:">+92 485 99 00</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
 </div>
 <?php include "inc/footer.inc"; ?>
