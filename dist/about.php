c18c3c4ce45ab4c4eed8fd36a48a20096063a094
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-08-10 14:20:24 +0200

    about.php development

---
+++ dist/about.php
@@ -0,0 +1,203 @@
+<?php include "inc/head.inc"; ?>
+<div class="page-about">
+	<div class="nav-bar dark">
+		<?php include "inc/nav.inc"; ?>
+	</div>
+	<section class="about-header">
+		<div class="about-header-inner" style="background-image: url('images/about/about_header.jpg');">
+			<div class="about-header-heading text-center">
+				<h1 class="relative">Auditori d'Alcúdia <span class="yellow-right-line"></span></h1>
+			</div>
+			<div class="about-header-content">
+				<div class="grid-container">
+					<div class="grid-x grid-padding-x align-right align-bottom">
+						<div class="small-12 medium-4 large-3 cell">
+							<div class="about-description">
+								<div class="about-description-heading">
+									<h2>Modernitat i tradició</h2>
+								</div>
+								<div class="about-description-body">
+									<p>A Alcúdia una vegada més s’uneix la modernitat i la tradició, i un exemple d’aquesta simbiosi és l’Auditori d’Alcúdia. Edifici inaugurat l’any 2000 es mostra com un balcó a la ciutat medieval, des del qual es pot contemplar la porta de Sant Sebastià o de Mallorca que ens convida a entrar-hi.</p>
+									<p>Una nova projecció cultural de la ciutat d’Alcúdia ha estat possible gràcies a la presència de l’Auditori d’Alcúdia i no només per als residents sinó també per als visitants i tota la població de la zona nord de Mallorca. Ha estat un repte important per a una ciutat que creu en la cultura com a base del desenvolupament humà i social  i que té una tradició marcada per la seva història i el seu patrimoni que ha de deixar pas a noves generacions que encara estan per arribar. </p>
+								</div>
+							</div>
+						</div>
+						<div class="small-12 medium-4 large-3 cell">
+							<div class="about-description-2">
+								<p>L’Auditori d’Alcúdia, és  el nou  equipament cultural de la ciutat  por excel·lència, un emblema de la contemporaneïtat.</p>
+								<p>L’Auditori d’Alcúdia, és un edifici polivalent que està al servei  dels   ciutadans  i visitants per gaudir de la cultura. Es caracteritza per tenir una programació estable al llarg de tot l’any d’arts escèniques i musicals i és la seu de  l’0Escola Municipal de Música d’Alcúdia</p>
+								<p>L’ oferta cultural en arts escèniques  i musicals és àmplia, aquest  espai  permet programar tot tipus de manifestacions culturals:  música clàssica, circ, jazz,  teatre, dansa, cinema, audiovisuals...</p>
+								<hr>
+								<div class="about-next-events">
+									<p><strong>Entre la programació que realitza al llarg de l’any podem destacar: </strong></p>
+									<div class="about-next-events-list">
+										<div class="about-next-events-item">
+											<h3>SETEMBRE</h3>
+											<p>Festival de Jazz “Alcúdia Jazz”.</p>
+										</div>
+										<div class="about-next-events-item">
+											<h3>SETEMBRE</h3>
+											<p>Festival de Jazz “Alcúdia Jazz”.</p>
+										</div>
+										<div class="about-next-events-item">
+											<h3>SETEMBRE</h3>
+											<p>Festival de Jazz “Alcúdia Jazz”.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="about-header-gallery section-padding">
+				<div class="grid-container">
+					<div class="grid-x grid-padding-x">
+						<div class="cell small-6 medium-3">
+							<div class="gallery-item-1">
+								<img src="images/placeholder/about_gallery_placeholder.jpg">
+							</div>
+							<div class="gallery-item-5">
+								<img src="images/placeholder/about_gallery_placeholder.jpg">
+							</div>
+						</div>
+						<div class="cell small-6 medium-3">
+							<div class="gallery-item-1">
+								<img src="images/placeholder/about_gallery_placeholder.jpg">
+							</div>
+							<div class="gallery-item-5">
+								<img src="images/placeholder/about_gallery_placeholder.jpg">
+							</div>
+						</div>
+						<div class="cell small-6 medium-3">
+							<div class="gallery-item-1">
+								<img src="images/placeholder/about_gallery_placeholder.jpg">
+							</div>
+							<div class="gallery-item-5">
+								<img src="images/placeholder/about_gallery_placeholder.jpg">
+							</div>
+						</div>
+						<div class="cell small-6 medium-3">
+							<div class="gallery-item-1">
+								<img src="images/placeholder/about_gallery_placeholder.jpg">
+							</div>
+							<div class="gallery-item-5">
+								<img src="images/placeholder/about_gallery_placeholder.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="about-bookings-info section-primary">
+		<div class="grid-container">
+			<div class="grid-x text-center align-center">
+				<div class="cell small-9">
+					<h3>Vols reservar qualque espai de l’Auditori d’Alcúdia per espectacles, conferències, exposicions, etc?</h3>
+					<hr>
+					<a href="#" class="button button-dark">Reserva</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="about-cultura section-padding">
+		<div class="about-cultura-heading text-center">
+			<h2 class="relative"><span class="yellow-left-line"></span> Auditori d'Alcúdia = Compromís + <strong>Cultura</strong></h2>
+		</div>
+		<div class="about-cultura-content grid-container">
+			<div class="grid-x grid-padding-x">
+				<div class="cell medium-6 small-12 text-right">
+					<img src="images/placeholder/cultura_placeholder.jpg" alt="">
+				</div>
+				<div class="cell medium-6 small-12">
+					<p>L’Auditori d’Alcúdia està a prop dels ciutadans sense exclusió i destaca la seva labor a favor de col·lectius fràgils per això podem trobar al llarg de tot l’any un extens programa de concerts i actuacions de caire solidari.</p>
+					<div class="callout">
+						<p><strong>Algunes entitat amb les que fem feina són:</strong></p>
+						<p>Associació Amics del poble Saharauí<br>
+						Aspanob<br>
+						Associació Down i Més<br>
+						Associació per Ells<br>
+						Associació ABAIMAR<br>
+						Centre d’Educació Especial  Joan XXIII<br>
+						Projecte Home...</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="about-workshops section-padding">
+		<div class="about-cultura-heading text-center">
+			<h2 class="relative"><span class="yellow-right-line"></span>Tallers de Teatre de l’Auditori d’Alcúdia <br>per infants, joves i adults</strong></h2>
+		</div>
+		<div class="about-workshops-content grid-container">
+			<div class="grid-x grid-padding-x">
+				<div class="cell medium-6 small-12 text-right">
+					<p>L’Ajuntament d’Alcúdia organitza des de fa molts d’anys  tallers de teatre TTdA per a tots els segments de la població entre  els mesos de novembre a  maig i dins el programa Tardor Cultural.</p>
+					<p>Aquest tallers se realitzen amb l’objectiu  d’apropar les arts escèniques a la població i que sigui el teatre un element actiu i no passiu. El ciutadà no és només tractat com un espectador sinó com a part important del fet cultural, que participa activament en la programació teatral de la ciutat. Els tallers donen lloc a petites i mitjanes produccions que posteriorment se representen com a part important de la programació al llarg de tot l’any.</p>
+				</div>
+				<div class="cell medium-6 small-12">
+					<img src="images/about/about_header.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="contact-section section-padding section-light-gray">
+		<div class="contact-heading text-center">
+			<h2>Contacte</h2>
+		</div>
+		<div class="contact-content section-padding">
+			<div class="grid-container grid-container-padded">
+				<div class="grid-x grid-padding-x callout">
+					<div class="medium-6 cell medium-text-right text-center">
+						<a href="mailto:info@auditorialcudia.net" target="_blank">
+							<i class="fa fa-envelope"></i> info@auditorialcudia.net
+						</a>
+						<span class="separator hide-for-small-only">/</span>
+					</div>
+					<div class="medium-6 cell medium-text-left text-center">
+						<a href="tel:917897185">
+							<i class="fa fa-phone"></i> 971 89 71 85
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="contact-form">
+			<div class="row column">
+				<form id="contact-form" action="" method="POST">
+					<div class="row">
+						<div class="medium-4 columns">
+							<input type="text" name="name" placeholder="Nom">
+						</div>
+						<div class="medium-4 columns">
+							<input type="email" name="email" placeholder="Email">
+						</div>
+						<div class="medium-4 columns">
+							<input type="text" name="phone" placeholder="Telèfon">
+						</div>
+					</div>
+					<div class="row">
+						<div class="column">
+							<textarea name="message" placeholder="Missatge" rows="4"></textarea>
+						</div>
+					</div>
+					<div class="row">
+						<div class="column text-center">
+							<button type="submit" class="button">Enviar</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</section>
+	<section class="event-more section-padding section-primary section-padding">
+		<div class="row">
+			<div class="column text-center aling-center">
+				<a href="#" class="button button-dark">Veure tots els espectacles</a>
+			</div>
+		</div>
+	</section>
+	<?php include "inc/footer.inc"; ?>
+</div>
+<?php include "inc/bottom.inc"; ?>
