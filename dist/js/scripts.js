3acdacedd7c33ad9f7855b6ee130689ec9d9c089
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-08-16 15:16:06 +0200

    Dev seats map

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -84,3 +84,31 @@ jQuery(document).ready(function ($) {
     });
 
 });
+jQuery(document).ready(function ($) {
+
+    // Seats tooltip
+
+    $('*[data-seat-row]').each(function (index, el) {
+        var file = $(this).data('file');
+        if ($(this).data('row-wheelchair')) {
+            $(this).prepend('<div class="wheelchair-row"><i class="fa fa-wheelchair"></i></div>');
+        };
+        $(this).find('*[data-seat]').each(function (index, el) {
+            if ($(this).data('num') != undefined) {
+                var $tooltip = $('<div />').attr('class', 'seat-tooltip').css('display', 'none');
+                var num = $(this).data('num');
+                if ($(this).data('full')) {
+                    $(this).addClass('seat-full')
+                    $tooltip.addClass('seat-full').append('Ocupada');
+                } else {
+                    $tooltip.append('Fila ' + '<strong>' + file + '</strong> N° <strong>' + num + '</strong>');
+                }
+                if ($(this).data('wheelchair')) {
+                    $tooltip.prepend('<i class="fa fa-wheelchair"></i>&nbsp;');
+                }
+
+                $(this).append($tooltip);
+            }
+        });
+    });
+});
---
+++ dist/seats-map.php
@@ -0,0 +1,216 @@
+<?php include "inc/head.inc"; ?>
+<div class="page-seats-map">
+	<div class="nav-bar dark">
+		<?php include "inc/nav.inc"; ?>
+	</div>
+	<section class="seats-teaser section-padding section-dark">
+		<div class="row seats-teaser-inner">
+			<div class="column large-2 small-4 seats-teaser-image">
+				<img src="http://via.placeholder.com/350x350" alt="">
+			</div>
+			<div class="column large-4 small-8 seats-teaser-info">
+				<div class="seats-teaser-heading">
+					<h2>Praesent sapien massa, convallis a pellentesque nec.</h2>
+				</div>
+				<div class="grid-x grid-x-padding">
+					<div class="cell small-4 seats-teaser-date">
+						<i class="fa fa-long-arrow-right"></i>&nbsp;DIL <strong>01 MAR</strong>
+					</div>
+					<div class="cell small-4 seats-teaser-time">
+						<i class="fa fa-clock-o"></i>&nbsp; <strong>18:00</strong>h
+					</div>
+					<div class="cell small-4 seats-teaser-location">
+						<i class="fa fa-map-marker"></i>&nbsp;Auditori d'Alúdia
+					</div>
+				</div>
+				<a href="#" class="seats-teaser-more-link">Modificar <i class="fa fa-chevron-right"></i></a>
+			</div>
+		</div>
+	</section>
+	<section class="seats-filter section-dark">
+		<div class="seats-filter-inner row">
+			<div class="small-12 medium-3 large-3 column text-center medium-text-left">
+				<p class="primary-color no-margin"><strong>Selecciona una o vàries butaques</strong></p>
+			</div>
+			<div class="small-12 medium-9 large-8 large-offset-1 column text-center medium-text-right">
+				<div class="grid-x grid-padding-x align-right">
+					<div class="cell medium-2"><strong><span class="primary-color">Referències: </span></strong></div>
+					<div class="cell medium-2">
+						<i class="seat-icon"></i>&nbsp;<span>Lliure</span>
+					</div>
+					<div class="cell medium-2">
+						<i class="seat-icon-full"></i>&nbsp;<span>Ocupada</span>
+					</div>
+					<div class="cell medium-2">
+						<i class="seat-icon-selected"></i>&nbsp;<span>Seleccionada</span>
+					</div>
+					<div class="cell medium-4">
+						<i class="fa fa-wheelchair"></i> Butaques amb mobilitat reduïda
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="seats-map section-padding section-dark">
+		<div class="grid-container">
+			<div class="grid-x grid-x-padding">
+				<div class="seats-map-seats small-12 large-8 cell">
+					<div class="seats-map-seats-block">
+						<div class="seats-block-fullarea">
+							<div class="seats-block-amphitheater seats-block-area">
+								<div class="area-name">Anfiteatre</div>
+								<div class="seats-row" data-seat-row data-row-wheelchair="true" data-file="1">
+									<a href="#" class="seat" data-seat data-num="1" data-wheelchair="true"><i class="seat-icon"></i></a>
+									<a href="#" class="seat" data-seat data-num="2" data-full="true"><i class="seat-icon"></i></a>
+									<a href="#"><i class="seat-icon"></i></a>
+									<a href="#" class="seat-selected"><i class="seat-icon"></i></a>
+									<a href="#"><i class="seat-icon"></i></a>
+									<a href="#"><i class="seat-icon"></i></a>
+									<a href="#"><i class="seat-icon"></i></a>
+								</div>
+								<div class="seats-row">
+									<div class="left">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="center">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="right">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+								</div>
+							</div>
+							<div class="seats-block-platea seats-block-area">
+								<div class="area-name">Platea</div>
+								<div class="seats-row">
+									<div class="left">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="center">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="right">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+								</div>
+								<div class="seats-row">
+									<div class="left">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="center">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="right">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+								</div>
+								<div class="seats-row">
+									<div class="left">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="center">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="right">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+								</div>
+								<div class="seats-row">
+									<div class="left">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="center">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+									<div class="right">
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+										<a href="#"><i class="seat-icon"></i></a>
+									</div>
+								</div>
+							</div>
+							<div class="seats-block-stage text-center">
+								<div class="stage-box">Escenari</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="seats-map-tickets small-12 large-4 cell">
+					<div class="seats-map-tickets-block">
+						Entrades
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php include "inc/footer.inc"; ?>
+</div>
+<?php include "inc/bottom.inc"; ?>
