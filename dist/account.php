8a671127c55c9f9c360433159f4df2c38ee5a2f1
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-08-21 12:35:56 +0200

    My account dev

--- dist/account.php
+++ dist/account.php
@@ -10,44 +10,44 @@
 		<div class="my-account-form-body">
 			<div class="grid-container">
 				<div class="grid-x grid-padding-x align-center">
-					<div class="cell small-12 medium-10 large-10">
+					<div class="cell small-12 large-10">
 						<div class="my-account-form-block">
 							<form id="my-account-form" action="" method="POST">
-								<div class="form-message alert row column">Dades incorrectes. Revisa-ho</div>
+								<div class="form-message success row column">Les dades han estat guardades correctament</div>
 								<div class="row">
-									<div class="column medium-4">
+									<div class="column small-12 medium-4">
 										<label class="show-for-sr">Nom</label>
 										<input type="text" name="name" value="" placeholder="Nom" required>
 									</div>
-									<div class="column medium-4">
+									<div class="column small-12 medium-4">
 										<label class="show-for-sr">Email</label>
 										<input type="email" name="email" value="" placeholder="Email" required>
 									</div>
-									<div class="column medium-4">
+									<div class="column small-12 medium-4">
 										<label class="show-for-sr">Telèfon</label>
 										<input type="text" name="email" value="" placeholder="Telèfon">
 									</div>
 								</div>
 								<div class="row">
-									<div class="column medium-8">
+									<div class="column small-12 medium-8">
 										<label class="show-for-sr">Nom</label>
 										<input type="text" name="address" value="" placeholder="Direcció postal">
 									</div>
-									<div class="column medium-4">
+									<div class="column small-12 medium-4">
 										<label class="show-for-sr">Codi postal</label>
 										<input type="text" name="postalcode" value="" placeholder="Codi postal">
 									</div>
 								</div>
 								<div class="row">
-									<div class="column medium-4">
+									<div class="column small-12 medium-4">
 										<label class="show-for-sr">Població</label>
 										<input type="text" name="town" value="" placeholder="Població">
 									</div>
-									<div class="column medium-4">
+									<div class="column small-12 medium-4">
 										<label class="show-for-sr">Clau d'accés</label>
 										<input type="password" name="password" value="" placeholder="Clau d'accés">
 									</div>
-									<div class="column medium-4">
+									<div class="column small-12 medium-4">
 										<label class="show-for-sr">Clau d'accés</label>
 										<input type="password" name="validate-password" value="" placeholder="Confirma la clau d'accés">
 									</div>
