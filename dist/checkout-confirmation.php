4a9cae4ee0b0914d3835ec46a970eb8241406e21
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-08-24 10:40:20 +0200

    Checkout confirmation dev

---
+++ dist/checkout-confirmation.php
@@ -0,0 +1,31 @@
+
+<?php include "inc/head.inc"; ?>
+<div class="page-checkout-confirmation">
+	<div class="nav-bar dark">
+		<?php include "inc/nav.inc"; ?>
+	</div>
+	<section class="basic-section section-padding">
+		<div class="basic-section-heading heading-padding text-center">
+			<h1 class="section-title"><span class="yellow-left-line"></span>Compra confirmada</h1>
+		</div>
+		<div class="basic-section-content row">
+			<div class="column">
+				<div class="field-body text-center">
+					<p>La teva compra s’ha realitzat correctament. En breu rebràs les entrades per correu electrònic. Si no les rebs durant els propers minuts, revisa la teva bústia d’entrada.</p>
+					<p>Sempre podràs recuperar les teves entrades accedint al teu historial d’entrades comprades.</p>
+					<br>
+					<a href="#" class="button button-primary">Historial d'entrades</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="event-more section-padding section-primary section-padding">
+		<div class="row">
+			<div class="column text-center aling-center">
+				<a href="#" class="button button-dark">Veure tots els espectacles</a>
+			</div>
+		</div>
+	</section>
+	<?php include "inc/footer.inc"; ?>
+</div>
+<?php include "inc/bottom.inc"; ?>
