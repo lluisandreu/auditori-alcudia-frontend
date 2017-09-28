5463216217a4d2f92e41e478825d962c564a9553
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-09-28 11:40:43 +0200

    Pàgina 500

---
+++ dist/500.php
@@ -0,0 +1,26 @@
+<?php include "inc/head.inc"; ?>
+<div class="page-basic">
+	<div class="nav-bar dark">
+		<?php include "inc/nav.inc"; ?>
+	</div>
+	<section class="basic-section section-padding">
+		<div class="basic-section-heading heading-padding text-center">
+			<h1 class="section-title"><span class="yellow-left-line short"></span> Error 500. Error intern del servidor.</h1>
+		</div>
+		<div class="basic-section-content row">
+			<div class="column">
+				<div class="field-body">
+					<div class="text-center">
+						<h2>Ens sap greu! Pareix que tenim un problema intern que aviat solucionarem!</h2>
+						<p>Per favor, actualitza la pàgina. Si el problema persisteix, t'agrairïem que ens comuniquis l'error. Disculpa les molèsties.</p>
+						<br>
+						<a href="mailto:soporte@ditsticketing.com?subject=Error 500 a la web de l'Auditori d'Alcúdia" class="button button-primary">Informar de l'error</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<?php include "inc/footer.inc"; ?>
+</div>
+<?php include "inc/bottom.inc"; ?>
